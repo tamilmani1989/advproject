670f004ef2674c5cc5434ac3a458adccfab6dc85
tamilmani <tamilmani@compas175.compas.cs.stonybrook.edu>
2016-12-22 23:11:32 -0500

    cleaned up test code

--- test/lruk/temp.c
+++ test/lruk/temp.c
@@ -7,6 +7,7 @@
 #include <sys/mman.h>
 #include <sys/resource.h>
 #include<stdlib.h>
+#include<errno.h>
 #include <sys/mman.h>
 
 int main(int argc, char const *argv[])
@@ -17,8 +18,9 @@ int main(int argc, char const *argv[])
     	const char * file_name = argv[1];
 	struct rusage rusage;
 	unsigned long start;
-	char c;
-	system("sudo su -c \"echo 3 > /proc/sys/vm/drop_caches\"");
+       	char c=0;
+	int i=0;
+//	system("sudo su -c \"echo 3 > /proc/sys/vm/drop_caches\"");
 
 	getrusage(RUSAGE_SELF, &rusage);
         start = rusage.ru_majflt;
@@ -29,71 +31,73 @@ int main(int argc, char const *argv[])
     	/* Get the size of the file. */
     	status = fstat (fd1, & s);
     	size1 = s.st_size;
-
-
-//	printf("ret:%d\n",ret);
-
-
+//        int ret = posix_fadvise(fd1, 0, 0, POSIX_FADV_RANDOM );
+//        printf("Ret :%d\n",ret);
 
     	f1 = (char *) mmap (0, size1, PROT_READ, MAP_SHARED, fd1, 0);
-        int ret = madvise(f1, size1, MADV_RANDOM);
-        printf("Ret:%d\n",ret);
-
-	
-//	int ret = madvise(f1, 0, MADV_RANDOM);
-//	printf("Ret:%d\n",ret);
+	int ret = madvise(f1, size1, MADV_RANDOM);
 
 
-//	f1=malloc(size1);
-	unsigned long len = (size1*3)/4;
-
-
-	printf("size:%lu %d\n",size1,sizeof(unsigned long));
-
-	for(k=0;k<1;k++)
+	if(f1==MAP_FAILED)
 	{
-    		for (unsigned long i = size1/2; i < len; i=i+4096) {
-//		for (unsigned long i = 0; i < 12189; i=i+4096) {
+		printf("MAP failed:%d\n",errno);
+	}
+
+	//unsigned long len = (size1*3)/4;
+
+/*	for(unsigned long i=0;i<size1/4;i++)
+	{
+		c+=f1[i];
+	}
+*/
+/*	for(k=0;k<1;k++)
+	{
+    		for (unsigned long i = 0; i <size1/3; i=i+4096) {
 
         		c+=f1[i];
+			//if(c >=0 && c<128)
     		}
+//		sleep(10);
 	}
-#if 1
-        for(k=0;k<4;k++)
-        {
-                for (unsigned long i = 0; i < size1/2; i=i+4096) {
-                        char c;
-
-                        c=f1[i];
-                }
-        }
 
         for(k=0;k<1;k++)
         {
-                for (unsigned long i = 0; i < len; i=i+4096) {
-                        char c;
+                for (unsigned long i = 0; i <size1/2; i=i+4096) {
 
-                        c=f1[i];
+                        c+=f1[i];
+			//if(c >=0 && c <128)
                 }
         }
+*/
+#if 1
 
-
-        for(k=0;k<3;k++)
+        for(k=0;k<2;k++)
         {
-                for (unsigned long i = 0; i < size1/2; i=i+4096) {
+                for (unsigned long i = 0; i < 2147483648 ; i=i+4096) {
+                        c+=f1[i];
+		}
+			//if(c>=0 && c<128)
+	}
+#endif
+
+#if 0
+
+        for(k=0;k<1;k++)
+        {
+                for (unsigned long i = 0; i < size1/3; i=i+4097) {
                         char c;
 
-                        c=f1[i];
+                        c+=f1[i];
                 }
         }
 
 #endif
+
        getrusage(RUSAGE_SELF, &rusage);
         printf("%ld\n",rusage.ru_majflt-start);
 
 
-	//munmap(f1,size1);
-//	free(f1);
+	munmap(f1,size1);
 	close(fd1);
 
     	return 0;
--- test/lruk/testscript.sh
+++ test/lruk/testscript.sh
@@ -1,52 +1,47 @@
-for k in `seq 1 20`
+for k in `seq 1 30`
 do
 	sudo sysctl kernel.sample=1
 	sudo su -c "echo 3 >> /proc/sys/vm/drop_caches"
 	start=$(date +"%s")
-	echo $start
-	./freq2 3g.txt
+	./freq2 $1
 	sleep 10
-	./freq2 3g.txt
+	./freq2 $1
 	sleep 10
 	for i in `seq 1 4`;
 	do
-		./freq 3g.txt
+		./freq $1
 		sleep 1
 	done
 	sleep 1
-	./freq2 3g.txt
+	./freq2 $1
 
-	./temp 3g.txt >> results-lru.txt
-	./freq 3g.txt >> results-lru.txt
+	./temp $1
+	./freq $1
 	end=$(date +"%s")
-	echo "LRU $end  $start"
 	time=`expr $end - $start`
 	echo $time >> results-lru.txt
-	echo "------" >> results-lru.txt
 	
 	sudo sysctl kernel.sample=2
 	sudo sysctl kernel.lruk=10
 	sudo su -c "echo 3 >> /proc/sys/vm/drop_caches"
 	start=$(date +"%s")
-        ./freq2 3g.txt
+        ./freq2 $1
         sleep 10
-        ./freq2 3g.txt
+        ./freq2 $1
         sleep 10
         for i in `seq 1 4`;
         do
-                ./freq 3g.txt
+                ./freq $1
                 sleep 1
         done
         sleep 1
-        ./freq2 3g.txt
+        ./freq2 $1
 
-        ./temp 3g.txt >> results-lruk.txt
-        ./freq 3g.txt >> results-lruk.txt
+        ./temp $1
+        ./freq $1
 	end=$(date +"%s")
         time=`expr $end - $start`
         echo $time >> results-lruk.txt
-	echo "LRU_K $end $start"
-	echo "-------" >> results-lruk.txt
 
 done
 
